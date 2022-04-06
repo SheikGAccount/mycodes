bb2a66d2d9856eac8e53cc7752d744b2df12c23f
SheikGAccount <sheikabdulkadar12@gmail.com>
2022-04-06 20:24:40 +0530

    adding sample file with initial commit

---
+++ NumberInString/src/NumberInString/NumberInString.java
@@ -0,0 +1,38 @@
+package NumberInString;
+
+public class NumberInString {
+
+	  public static void main(String[] args) {
+	        System.out.println("Hello, World!"); 
+	 String s = "78,67,87,-1,10,12,0";
+	       String s1[] = s.split(",");
+	       int[] n= new int[10];
+	       int temp;
+	       System.out.println(" Before parsing Int");
+	       for (int i=0; i<s1.length; i++)
+	       {
+	           n[i] = Integer.parseInt(s1[i]);
+	          
+	       }
+	       System.out.println(" Before comparison");
+	       for (int i=0; i<n.length; i++)
+	       {
+	           for(int j=1; j<n.length; j++)
+	           {
+	               if(n[i]>n[j])
+	               {
+	                   temp = n[i];
+	                   n[i]=n[j];
+	                   n[j]=n[i];
+	               }
+	           }
+	       } 
+	       System.out.println(" Before Printing");
+	       for(int i=0;i<n.length; i++)
+	       {
+	       System.out.println("Values in Order" + n[i]);
+	       }
+	      }
+	}
+
+
