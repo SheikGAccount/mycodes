b416e2eca06de74ba8c645c9d80d51aa990f400c
SheikGAccount <47471894+SheikGAccount@users.noreply.github.com>
2023-08-14 23:24:34 +0530

    Update Test.java

--- NumberInString/src/NumberInString/Test.java
+++ NumberInString/src/NumberInString/Test.java
@@ -1,5 +1,10 @@
 package NumberInString;
 
 public class Test {
+  public static void main(String[] args){
+    int a=5,b=10;
+	System.out.println("Sample Test");
+	System.out.println(a+b);
+  }
 
 }
